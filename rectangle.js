e727d267ba2222ef61f979f6bf1042db12f11a3a
Aitor Bernal <alu0100830064@ull.edu.es>
2017-04-23 11:18:43 +0100

    Subiendo la clase

---
+++ rectangle.js
@@ -0,0 +1,14 @@
+/**
+ * Created by chinegua on 23/4/17.
+ */
+class Rectangle extends Figura {
+    constructor(options) {
+        Shapes.Rectangle = Rectangle;
+        super("Rectangle",options)
+        this.width = options.width;
+        this.height = options.height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+}
